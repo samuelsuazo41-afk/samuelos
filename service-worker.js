b41d7aadd67acc243dd1679b32343e2e4f2a16f2
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-09 16:26:53 +0200

    Crear service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', e => {
+  e.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
