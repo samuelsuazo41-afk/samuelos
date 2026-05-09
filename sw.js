49afdb10f78877f2bf32196c0ff80218c9af2051
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-09 16:34:38 +0200

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'samuel-os-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
